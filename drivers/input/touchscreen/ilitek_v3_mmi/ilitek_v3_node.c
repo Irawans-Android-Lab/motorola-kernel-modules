00efd9bbaa925720fbca12442c8bf824afc8e7d4
longc <longc154@motorola.com>
2024-06-13 14:32:00 +0800

    vegas: 1st ilitek_v3_mmi driver bringup
    vegas: 1st ilitek tp driver bringup
    add vegas tp driver
    
    Change-Id: I069850f54af925d9840fddc7ae72a435dae4d5dc
    Reviewed-on: https://gerrit.mot.com/2999354
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jun Weng <wengjun1@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/ilitek_v3_mmi/ilitek_v3_node.c
+++ drivers/input/touchscreen/ilitek_v3_mmi/ilitek_v3_node.c
@@ -2513,9 +2513,11 @@ static ssize_t ilitek_node_ioctl_write(struct file *filp, const char *buff, size
 
 	}else if (strncmp(cmd, "disableicemode", strlen(cmd)) == 0) {
 		bool mcu;
+#if (TDDI_INTERFACE == BUS_I2C)
 		u8 mode;
-		mcu = data[1] & BIT(0);
 		mode = data[2] & 0x0F;
+#endif
+		mcu = data[1] & BIT(0);
 
 		if (ilits->cascade_info_block.nNum != 0) {
 #if (TDDI_INTERFACE == BUS_I2C)
