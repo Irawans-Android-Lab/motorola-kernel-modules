f98e601fd810d5bfebde21fce1f5f0cd658839e7
Lc_Wjm <longc166@motorola.com>
2024-06-17 14:53:36 +0800

    [vegas][charger] charger ic id comatible:modify charger ic macro control
    detail:
      do charger ic sgm41543d compatible with sgm41542
      if define charger ic sgm41543d macro,will define charger ic sgm41543d id macro and undefine sgm41542 id macro
    
    Change-Id: I6eaa47ed64c4aa840d896572c41e67c890cf296f
    Reviewed-on: https://gerrit.mot.com/3001879
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Reviewed-by: Yifan Xu <xuyf4@motorola.com>
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/sgm415xx_mmi/Android.mk
+++ drivers/power/sgm415xx_mmi/Android.mk
@@ -1,13 +1,8 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(CONFIG_MMI_SGM41543D_CHARGER),true)
-	KERNEL_CFLAGS += CONFIG_MMI_SGM41543D_CHARGER=y
-endif
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := sgm415xx_mmi.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-
 include $(DLKM_DIR)/AndroidKernelModule.mk
