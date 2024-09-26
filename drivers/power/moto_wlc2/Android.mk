462540a75c27f367573d81753045d88e9cca0847
Guocheng Wang <wanggc6@motorola.com>
2024-09-26 20:41:03 +0800

    moto_wlc2: Add moto_wlc2 build files
    Add some build files
    to build moto_wlc2.
    
    
    Change-Id: I75b028d7fec76ffe2ff7114d0c246ed1c059c721
    Signed-off-by: Guocheng Wang <wanggc6@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3098192
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/moto_wlc2/Android.mk
@@ -0,0 +1,9 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := moto_wlc2_alg.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
+include $(DLKM_DIR)/AndroidKernelModule.mk
