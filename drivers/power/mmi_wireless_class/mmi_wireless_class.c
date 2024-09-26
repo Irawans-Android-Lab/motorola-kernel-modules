ed9e3c0acafbfffe828a097356769af5cc99592e
Guocheng Wang <wanggc6@motorola.com>
2024-09-26 15:25:10 +0800

    mmi_wireless_class: Init mmi_wireless_class interface
    Init mmi_wireless_class interface
    for moto wlc 2.0.
    
    Change-Id: I4a5f7c8855c3f35468cd75be1b6b585bade0e53c
    Signed-off-by: Guocheng Wang <wanggc6@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3097865
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/mmi_wireless_class/mmi_wireless_class.c
@@ -0,0 +1,268 @@
+#include <linux/module.h>
+#include <linux/stat.h>
+#include <linux/init.h>
+#include <linux/ctype.h>
+#include <linux/err.h>
+#include <linux/slab.h>
+#include <linux/power_supply.h>
+
+#include <linux/mmi_wireless_class.h>
+
+#define to_wireless_device(obj) container_of(obj, struct wireless_device, dev)
+
+static struct class *wireless_class = NULL;
+
+#define WLS_RX_CHECK(x)\
+bool wls_rx_check_##x(struct wireless_device *wls_dev)\
+{\
+	if (wls_dev != NULL && wls_dev->rx_ops != NULL && wls_dev->rx_ops->check_##x)\
+		return wls_dev->rx_ops->check_##x(wls_dev);\
+	return false;\
+}\
+EXPORT_SYMBOL(wls_rx_check_##x);
+
+#define WLS_RX_GET(x,y)\
+int wls_rx_get_##x(struct wireless_device *wls_dev, y)\
+{\
+	if (wls_dev != NULL && wls_dev->rx_ops != NULL && wls_dev->rx_ops->get_##x)\
+		return wls_dev->rx_ops->get_##x(wls_dev, a);\
+	return -EOPNOTSUPP;\
+}\
+EXPORT_SYMBOL(wls_rx_get_##x);
+
+#define WLS_RX_SET(x,y)\
+int wls_rx_set_##x(struct wireless_device *wls_dev, y)\
+{\
+	if (wls_dev != NULL && wls_dev->rx_ops != NULL && wls_dev->rx_ops->set_##x)\
+		return wls_dev->rx_ops->set_##x(wls_dev, a);\
+	return -EOPNOTSUPP;\
+}\
+EXPORT_SYMBOL(wls_rx_set_##x);
+
+#define WLS_TX_CHECK(x)\
+bool wls_tx_check_##x(struct wireless_device *wls_dev)\
+{\
+	if (wls_dev != NULL && wls_dev->tx_ops != NULL && wls_dev->tx_ops->check_##x)\
+		return wls_dev->tx_ops->check_##x(wls_dev);\
+	return false;\
+}\
+EXPORT_SYMBOL(wls_tx_check_##x);
+
+#define WLS_TX_GET(x,y)\
+int wls_tx_get_##x(struct wireless_device *wls_dev, y)\
+{\
+	if (wls_dev != NULL && wls_dev->tx_ops != NULL && wls_dev->tx_ops->get_##x)\
+		return wls_dev->tx_ops->get_##x(wls_dev, a);\
+	return -EOPNOTSUPP;\
+}\
+EXPORT_SYMBOL(wls_tx_get_##x);
+
+#define WLS_TX_SET(x,y)\
+int wls_tx_set_##x(struct wireless_device *wls_dev, y)\
+{\
+	if (wls_dev != NULL && wls_dev->tx_ops != NULL && wls_dev->tx_ops->set_##x)\
+		return wls_dev->tx_ops->set_##x(wls_dev, a);\
+	return -EOPNOTSUPP;\
+}\
+EXPORT_SYMBOL(wls_tx_set_##x);
+
+
+WLS_RX_GET(chip_id, int *a);
+WLS_RX_GET(fw_version, int *a);
+WLS_RX_GET(rx_neg_power, int *a);
+WLS_RX_GET(op_mode, int *a);
+WLS_RX_GET(sys_mode, int *a);
+WLS_RX_GET(rx_die_temp, int *a);
+
+WLS_RX_GET(rx_irect, int *a);
+WLS_RX_GET(rx_iout, int *a);
+WLS_RX_GET(rx_vrect, int *a);
+WLS_RX_GET(rx_vout, int *a);
+WLS_RX_GET(rx_vout_setting, int *a);
+
+WLS_RX_SET(irq_enable, bool a);
+WLS_RX_SET(mode_select, bool a);
+WLS_RX_SET(fw_update, bool a);
+
+WLS_RX_CHECK(ldo_on);
+
+
+int wls_rx_send_ask_packet(struct wireless_device *wls_dev, uint8_t *data, int data_len)
+{
+	if (wls_dev != NULL &&
+		wls_dev->rx_ops != NULL &&
+		wls_dev->rx_ops->send_ask_packet)
+		return wls_dev->rx_ops->send_ask_packet(wls_dev, data, data_len);
+	return -EOPNOTSUPP;
+}
+EXPORT_SYMBOL(wls_rx_send_ask_packet);
+
+static ssize_t name_show(struct device *dev,
+					struct device_attribute *attr, char *buf)
+{
+	struct wireless_device *wls_dev = to_wireless_device(dev);
+
+	return snprintf(buf, 20, "%s\n",
+				wls_dev->props.alias_name ?
+				wls_dev->props.alias_name : "anonymous");
+}
+
+static DEVICE_ATTR_RO(name);
+
+static struct attribute *wireless_class_attrs[] = {
+	&dev_attr_name.attr,
+	NULL,
+};
+
+static const struct attribute_group wireless_group = {
+	.attrs = wireless_class_attrs,
+};
+
+static const struct attribute_group *wireless_groups[] = {
+	&wireless_group,
+	NULL,
+};
+
+static void wireless_device_release(struct device *dev)
+{
+	struct wireless_device *wls_dev = to_wireless_device(dev);
+
+	if (!IS_ERR_OR_NULL(wls_dev))
+		kfree(wls_dev);
+}
+
+struct wireless_device *wireless_device_register(const char *name,
+		struct device *parent, void *devdata,
+		const struct wls_rx_ops *rx_ops,
+		const struct wls_tx_ops *tx_ops,
+		const struct wireless_properties *props)
+{
+	struct wireless_device *wls_dev = NULL;
+	int rc;
+
+	pr_info("%s: name=%s\n", __func__, name);
+
+	if(IS_ERR_OR_NULL(wireless_class)) {
+		pr_err("%s: wireless_class is err or null\n", __func__);
+		return NULL;
+	}
+
+	wls_dev = kzalloc(sizeof(*wls_dev), GFP_KERNEL);
+	if (IS_ERR_OR_NULL(wls_dev)) {
+		pr_err("%s: wls_dev kzalloc failed\n", __func__);
+		return NULL;
+	}
+
+	wls_dev->dev.class = wireless_class;
+	wls_dev->dev.parent = parent;
+	wls_dev->dev.release = wireless_device_release;
+	dev_set_name(&wls_dev->dev,"%s", name);
+	dev_set_drvdata(&wls_dev->dev, devdata);
+
+	/* Copy properties */
+	if (props) {
+		memcpy(&wls_dev->props, props, sizeof(struct wireless_properties));
+	}
+	rc = device_register(&wls_dev->dev);
+	if (rc) {
+		kfree(wls_dev);
+		pr_err("%s device register failed!\n", __func__);
+		return ERR_PTR(rc);
+	}
+
+	if (!IS_ERR_OR_NULL(rx_ops))
+		wls_dev->rx_ops = rx_ops;
+
+	if (!IS_ERR_OR_NULL(tx_ops))
+		wls_dev->tx_ops = tx_ops;
+
+	pr_info("%s successfully, wireless_dev=%p\n", __func__, wls_dev);
+
+	return wls_dev;
+}
+EXPORT_SYMBOL(wireless_device_register);
+
+void wireless_device_unregister(struct wireless_device *wls_dev)
+{
+	if (IS_ERR_OR_NULL(wireless_class))
+		return;
+
+	if (IS_ERR_OR_NULL(wls_dev))
+		return;
+
+	device_unregister(&wls_dev->dev);
+}
+EXPORT_SYMBOL(wireless_device_unregister);
+
+bool wireless_dev_set_callback(struct wireless_device *wls_dev, void *callback_ops)
+{
+	if (IS_ERR_OR_NULL(wls_dev) || IS_ERR_OR_NULL(callback_ops))
+		return false;
+	wls_dev->callback_ops = callback_ops;
+	return (!IS_ERR_OR_NULL(wls_dev->callback_ops));
+}
+EXPORT_SYMBOL(wireless_dev_set_callback);
+
+int wls_dev_send_event(struct wireless_device *wls_dev, struct wls_event_msg *msg)
+{
+	if (IS_ERR_OR_NULL(wls_dev) ||
+		IS_ERR_OR_NULL(wls_dev->callback_ops)||
+		IS_ERR_OR_NULL(wls_dev->callback_ops->event_handler)) {
+		return -1;
+	}
+
+	return wls_dev->callback_ops->event_handler(wls_dev, msg);
+}
+EXPORT_SYMBOL(wls_dev_send_event);
+
+static int wireless_match_device_by_name(struct device *dev,
+	const void *data)
+{
+	const char *name = data;
+	return strcmp(dev_name(dev), name) == 0;
+}
+
+struct wireless_device *get_wireless_by_name(const char *name)
+{
+	struct device *dev;
+
+	if (IS_ERR_OR_NULL(wireless_class))
+		return NULL;
+
+	if (!name)
+		return NULL;
+
+	dev = class_find_device(wireless_class, NULL, name,
+				wireless_match_device_by_name);
+	pr_info("%s wireless_dev=%p\n", __func__, dev);
+
+	return dev ? to_wireless_device(dev) : NULL;
+}
+EXPORT_SYMBOL(get_wireless_by_name);
+
+static void __exit wireless_class_exit(void)
+{
+	class_destroy(wireless_class);
+}
+
+static int __init wireless_class_init(void)
+{
+	wireless_class = class_create(THIS_MODULE, "mmi_wireless_class");
+	if (IS_ERR(wireless_class)) {
+		pr_notice("Unable to create wireless class; errno = %ld\n",
+			PTR_ERR(wireless_class));
+		return PTR_ERR(wireless_class);
+	}
+	pr_info("%s wireless_class=%p\n", __func__, wireless_class);
+	wireless_class->dev_groups = wireless_groups;
+	return 0;
+}
+
+module_init(wireless_class_init);
+module_exit(wireless_class_exit);
+
+MODULE_DESCRIPTION("Mmi Wireless Class Device");
+MODULE_AUTHOR("Motorola Mobility LLC");
+MODULE_VERSION("1.0.0");
+MODULE_LICENSE("GPL");
+
---
+++ include/linux/mmi_wireless_class.h
@@ -0,0 +1,158 @@
+#ifndef _MMI_WIRELESS_CLASS_H
+#define _MMI_WIRELESS_CLASS_H
+
+#include <linux/kernel.h>
+#include <linux/device.h>
+#include <linux/mutex.h>
+
+//****************system mode***************
+#define SYS_MODE_BACK_POWER	1
+#define SYS_MODE_TX			2
+#define SYS_MODE_RX			3
+
+typedef enum
+{
+	WLS_EVENT_NULL = 0,
+	WLS_EVENT_RX_POWER_ON,
+	WLS_EVENT_RX_NEGO_POWER_READY,
+	WLS_EVENT_RX_LDO_ON,
+	WLS_EVENT_RX_LDO_OFF,
+	WLS_EVENT_HS_OK,
+	WLS_EVENT_HS_FAIL,
+	WLS_EVENT_RX_FSK_PKT,
+	WLS_EVENT_TX_DETECTED,//tx_detected
+	WLS_EVENT_MAX
+} WLS_EVENT_T;
+
+struct wls_event_msg
+{
+	WLS_EVENT_T event;
+	int len;
+	uint8_t data[32];
+};
+
+typedef enum {
+	Sys_Op_Mode_AC_Missing = 0,
+	Sys_Op_Mode_BPP = 0x1,
+	Sys_Op_Mode_EPP = 0x2,
+	Sys_Op_Mode_MOTO_WLC = 0x3,
+	Sys_Op_Mode_PDDE= 0x4,
+	Sys_Op_Mode_TX = 0x8,
+	Sys_Op_Mode_TX_FOD = 0x9,
+	Sys_Op_Mode_INVALID,
+} Sys_Op_Mode;
+
+struct wireless_device;
+
+struct wls_callback_ops {
+	int (*event_handler)(struct wireless_device *wls_dev, struct wls_event_msg *msg);
+};
+
+struct wls_rx_ops {
+	//rx sys info
+	int (*get_chip_id)(struct wireless_device *wls_dev, int *chip_id);
+	int (*get_fw_version)(struct wireless_device *wls_dev, int *fw_version);
+	int (*get_op_mode)(struct wireless_device *wls_dev, int *mode);
+	int (*get_sys_mode)(struct wireless_device *wls_dev, int *sys_mode);
+	int (*get_rx_die_temp)(struct wireless_device *wls_dev, int *temp);
+
+	//rx charging info
+	int (*get_rx_neg_power)(struct wireless_device *wls_dev, int *power);
+	int (*get_rx_irect)(struct wireless_device *wls_dev, int *cur);
+	int (*get_rx_iout)(struct wireless_device *wls_dev, int *cur);
+	int (*get_rx_vrect)(struct wireless_device *wls_dev, int *voltage);
+	int (*get_rx_vout)(struct wireless_device *wls_dev, int *voltage);
+	int (*get_rx_vout_setting)(struct wireless_device *wls_dev, int *voltage);
+
+	int (*set_rx_vout_target)(struct wireless_device *wls_dev, int voltage);
+	int (*set_irq_enable)(struct wireless_device *wls_dev, bool en);
+	int (*set_mode_select)(struct wireless_device *wls_dev, bool on);
+	int (*set_fw_update)(struct wireless_device *wls_dev, bool force);
+
+	bool (*check_ldo_on)(struct wireless_device *wls_dev);
+	bool (*check_rx_power_on)(struct wireless_device *wls_dev);
+	bool (*check_auth_handshake)(struct wireless_device *wls_dev);
+
+	int (*send_ask_packet)(struct wireless_device *wls_dev, uint8_t *data, int data_len);
+};
+
+struct wls_tx_ops {
+	//tx mode
+	int (*set_tx_mode)(struct wireless_device *wls_dev, bool tx_mode);
+	int (*get_tx_mode)(struct wireless_device *wls_dev, bool *tx_mode);
+
+	//tx settings
+	int (*get_tx_iout)(struct wireless_device *wls_dev, int *cur);
+	int (*get_tx_vout)(struct wireless_device *wls_dev, int *vol);
+	int (*get_tx_power)(struct wireless_device *wls_dev, int *power);
+};
+
+struct wireless_properties {
+	const char *alias_name;
+};
+
+struct wireless_device {
+	struct device dev;
+	struct wireless_properties props;
+	void *driver_data;
+
+	const struct wls_rx_ops *rx_ops;
+	const struct wls_tx_ops *tx_ops;
+	const struct wls_callback_ops *callback_ops;
+};
+
+static inline void *wireless_dev_get_drvdata(const struct wireless_device *wls_dev)
+{
+	if (IS_ERR_OR_NULL(wls_dev))
+		return NULL;
+	return wls_dev->driver_data;
+}
+
+static inline bool wireless_dev_set_drvdata(struct wireless_device *wls_dev, void *data)
+{
+	if (IS_ERR_OR_NULL(wls_dev))
+		return false;
+	wls_dev->driver_data = data;
+
+	return true;
+}
+
+static inline void *wireless_get_data( struct wireless_device *wls_dev)
+{
+	return dev_get_drvdata(&wls_dev->dev);
+}
+
+extern struct wireless_device *wireless_device_register(const char *name,
+		struct device *parent, void *devdata,
+		const struct wls_rx_ops *rx_ops,
+		const struct wls_tx_ops *tx_ops,
+		const struct wireless_properties *props);
+
+extern void wireless_device_unregister(struct wireless_device *wls_dev);
+
+extern struct wireless_device *get_wireless_by_name(const char *name);
+
+extern bool wireless_dev_set_callback(struct wireless_device *wls_dev, void *callback_ops);
+extern int wls_dev_send_event(struct wireless_device *wls_dev, struct wls_event_msg *msg);
+
+extern int wls_rx_get_chip_id(struct wireless_device *wls_dev, int *chip_id);
+extern int wls_rx_get_fw_version(struct wireless_device *wls_dev, int *fw_version);
+extern int wls_rx_get_rx_neg_power(struct wireless_device *wls_dev, int *power);
+extern int wls_rx_get_op_mode(struct wireless_device *wls_dev, int *op_mode);
+extern int wls_rx_get_sys_mode(struct wireless_device *wls_dev, int *sys_mode);
+
+extern int wls_rx_get_rx_irect(struct wireless_device *wls_dev, int *cur);
+extern int wls_rx_get_rx_iout(struct wireless_device *wls_dev, int *cur);
+extern int wls_rx_get_rx_vrect(struct wireless_device *wls_dev, int *voltage);
+extern int wls_rx_get_rx_vout(struct wireless_device *wls_dev, int *voltage);
+extern int wls_rx_get_rx_vout_setting(struct wireless_device *wls_dev, int *voltage);
+
+extern int wls_rx_set_irq_enable(struct wireless_device *wls_dev, bool en);
+extern int wls_rx_set_mode_select(struct wireless_device *wls_dev, bool on);
+extern int wls_rx_set_fw_update(struct wireless_device *wls_dev, bool force);
+
+extern bool wls_rx_check_ldo_on(struct wireless_device *wls_dev);
+
+extern int wls_rx_send_ask_packet(struct wireless_device *wls_dev, uint8_t *data, int data_len);
+
+#endif /*_MMI_WIRELESS_CLASS_H*/
