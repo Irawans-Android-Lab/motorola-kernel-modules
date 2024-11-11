809f3f888ad660d5d10e11c966f6659c12230c02
feiyu <tinno61@motorola.com>
2024-11-11 16:42:36 +0800

    focaltech_0flash_mmi_v2:add productinfo node
    focaltech_0flash_mmi_v2:
    focal touch add productinfo node for cqa
    
    Change-Id: I6cc26b042e871f9a578b2a6e527da1b1644595b6
    Reviewed-on: https://gerrit.mot.com/3141185
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/focaltech_0flash_mmi_v2/focaltech_ex_fun.c
+++ drivers/input/touchscreen/focaltech_0flash_mmi_v2/focaltech_ex_fun.c
@@ -1769,6 +1769,12 @@ static ssize_t ic_ver_show(struct device *dev,
 	return fts_ic_ver_show(dev, attr, buf);
 }
 
+static ssize_t productinfo_show(struct device *dev,
+        struct device_attribute *attr, char *buf)
+{
+        return fts_productinfo_show(dev, attr, buf);
+}
+
 static ssize_t debug_level_en_store(
     struct device *dev,
     struct device_attribute *attr, const char *buf, size_t count)
@@ -1952,6 +1958,7 @@ static struct device_attribute touchscreen_attributes[] = {
 	__ATTR_RO(path),
 	__ATTR_RO(vendor),
 	__ATTR_RO(ic_ver),
+	__ATTR_RO(productinfo),
 	__ATTR_RO(panel_supplier),
 #ifdef CONFIG_FTS_LAST_TIME
 	__ATTR_RO(timestamp),
