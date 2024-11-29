01b88178ed6bea004e65074761823bf023db2339
dengwei <dengwei1@motorola.com>
2024-11-29 13:26:07 +0800

    cybert: input touch add report rate feature
    add report rate
    and palm cancel
    
    Change-Id: Ia058c2cd3a1e917493dd3c09da8f6f9f75208710
    Signed-off-by: dengwei <dengwei1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3158234
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/focaltech_touch_v3_u_mmi/focaltech_ts_config.c
+++ drivers/input/touchscreen/focaltech_touch_v3_u_mmi/focaltech_ts_config.c
@@ -29,7 +29,7 @@ struct focaltech_ic_report_rate_config report_rate_config_info = {
 			.command = 1,
 		},
 	}
-#elif defined(PRODUCT_SCOUT)
+#elif defined(PRODUCT_SCOUT) || defined(PRODUCT_CYBERT)
 	.rate_config_count = 2,
 	.refresh_rate_ctrl = 0,
 	.interpolation_ctrl = 1,
